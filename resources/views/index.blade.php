ad1c31251821c43250824eb363a3afeb37019e2e
Afrina16 <nurafrinaalimi16@gmail.com>
2022-05-30 11:21:42 +0800

    side menu package

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main.app')
+
+<!DOCTYPE html>
+<html>
+
+<body>
+    @section('content')
+        <style>
+            .uper {
+                margin-top: 40px;
+            }
+
+        </style>
+        <h1>Test</h1>
+
+    </body>
+
+    </html>
+@endsection
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,4 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
+    <h1>Hello World!</h1>
 </div>
