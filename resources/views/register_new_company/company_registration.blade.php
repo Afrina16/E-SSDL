617fc776c4d92e41347c84bec3a834f8e9c5e1f4
Afrina16 <nurafrinaalimi16@gmail.com>
2022-05-30 16:50:45 +0800

    button

--- resources/views/register_new_company/company_registration.blade.php
+++ resources/views/register_new_company/company_registration.blade.php
@@ -8,32 +8,32 @@
             <form>
                 <div class="row mb-3">
                     <label for="inputCompanyName" class="col-sm-2 col-form-label">Company Name</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-6">
                         <input type="text" class="form-control" id="inputCompanyName" placeholder="Company Name">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="inputCompanyAddress" class="col-sm-2 col-form-label">Address</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-6">
                         <input type="text" class="form-control" id="inputCompanyAddress" placeholder="Company Address">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="inputCompanyCity" class="col-sm-2 col-form-label">City/Town</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-6">
                         <input type="text" class="form-control" id="inputCompanyCity" placeholder="Company City/Town">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="inputCompanyPostcode" class="col-sm-2 col-form-label">Postcode</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="text" class="form-control" id="inputCompanyPostcode" placeholder="EG : 15200">
                     </div>
                 </div>
 
                 <div class="row mb-3">
                     <label for="status" class="col-sm-2 col-form-label">State</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-6">
                         <select class="form-select" id="state" aria-label="Please Select">
                             <option selected disabled>Please Select</option>
                             <option value="1">Abu Dhabi</option>
@@ -70,49 +70,49 @@
 
                 <div class="row mb-3">
                     <label for="license" class="col-sm-2 col-form-label">License No.</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="text" class="form-control" id="license" placeholder="License No.">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="telephone" class="col-sm-2 col-form-label">Telephone</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="text" class="form-control" id="telephone" placeholder="Telephone">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="ext" class="col-sm-2 col-form-label">Ext</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="text" class="form-control" id="ext" placeholder="Ext">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="fax" class="col-sm-2 col-form-label">Fax</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="text" class="form-control" id="fax" placeholder="Fax">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="email" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-6">
                         <input type="email" class="form-control" id="email" placeholder="Email">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="contact" class="col-sm-2 col-form-label">Contact Person</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="number" class="form-control" id="contact" placeholder="Contact Person">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="mobile" class="col-sm-2 col-form-label">Mobile No.</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="number" class="form-control" id="mobile" placeholder="EX : 0199169169">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="codeActivity" class="col-sm-2 col-form-label">Code Activity</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-3">
                         <input type="number" class="form-control" id="codeActivity" placeholder="EX : 1/2/3/4/5/6/7">
                     </div>
                 </div>
@@ -128,13 +128,11 @@
                         </select>
                     </div>
                 </div> --}}
-             
+
                 <div class="row">
-                    <div class="col offset-sm-2">
-                        <button type="reset" class="btn btn-primary">Reset</button>
-                    </div>
-                    <div class="col offset-sm-2">
-                        <button type="submit" class="btn btn-primary">Search</button>
+                    <div class="col-xs-12 col-sm-12 col-md-12 offset-sm-2">
+                        <a href="" class="btn btn-secondary">Reset</a>
+                        <button type="submit" class="btn btn-primary"> Submit</button>
                     </div>
                 </div>
             </form>
